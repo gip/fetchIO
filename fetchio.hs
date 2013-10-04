8243f08d80c78fbd0a94c6639bec09954ccb3d18
Gilles Pirio <g36130@gmail.com>
2013-10-04 09:13:21 -0700

    Timeout

--- fetchio.hs
+++ fetchio.hs
@@ -34,17 +34,17 @@ output l m = putStrLn ("chsrap: " ++ (p l) ++ ": " ++ m)
 
 logger m = do
   t <- getClockTime
-  putStrLn $ (show t) ++ ": " ++ m
+  putStrLn $ "[" ++ (show t) ++ "] " ++ m
 
 main = do
   args <- getArgs
   cfg <- (liftM $ decode) (BL.readFile $ Prelude.head args)
   if isJust cfg 
   then
-  	let cfg' = fromJust cfg in
+    let cfg' = fromJust cfg in
     start cfg'
   else
-  	output Err "Couldn't read configuration"
+    output Err "Couldn't read configuration"
   return ()
 
 start cfg = do 
@@ -84,7 +84,7 @@ simpleFetch tchan
            else do
            	  conno <- openConnection (T.unpack out_h) "/" (fromMaybe "" out_login) (fromMaybe "" out_passw)
            	  openChannel conno
-  mng <- newManager def -- { managerResponseTimeout = Just 30 }
+  mng <- newManager $ def { managerResponseTimeout = Just 15000000 }
   loop chan chano mng
   return ()
   where
@@ -130,7 +130,7 @@ pop c q = do
 fetch proxy mng url he = do
   req <- parseUrl url 
   t0 <- getClockTime
-  let req0 = req { checkStatus = \_ _ _-> Nothing, responseTimeout = Just 15000000 }
+  let req0 = req { checkStatus = \_ _ _-> Nothing }
   let req1 = case proxy of ("localhost", 80, Nothing, Nothing) -> req0
                            (h,p, _, _) -> addProxy (encodeUtf8 h) p req0
   let req2 = req1 { requestHeaders = he ++ (requestHeaders req1) }
