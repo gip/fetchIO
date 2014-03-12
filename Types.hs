79a6808a982f7fc589dba3d62370551ce4967244
Gilles Pirio <g36130@gmail.com>
2014-03-12 16:52:40 -0700

    Do not show user/pass on log

--- Types.hs
+++ Types.hs
@@ -13,7 +13,14 @@ data Endpoint = Endpoint { host :: Maybe Text,
                            port :: Maybe Int,
                            user :: Maybe Text,
                            pass :: Maybe Text 
-                         } deriving (Generic, Show)
+                         } deriving (Generic)
+
+instance Show Endpoint where
+  show ep = Prelude.concat ["{host = ", show $ host ep, ", port = ", show $ port ep, 
+                            ", user = ", show $ f (user ep), ", pass = ", show $ f (pass ep)]
+    where
+      f (Just _) = "<redacted>"
+      f _ = "<none>" 
 
 getHost (Endpoint (Just h) _ _ _) = h
 getHost (Endpoint Nothing _ _ _) = "localhost"
